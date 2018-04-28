0dee89dc11c400b9f5dcd35d2ed9b00df25ec773
durell96 <38799569+durell96@users.noreply.github.com>
2018-04-27 20:27:45 -0400

    Changed IMG from local path...Changed db file

--- dbconnection.php
+++ dbconnection.php
@@ -1,14 +1,10 @@
 
   	<?php 
-  	$name = $_POST['name'];
-  	$comment = $_POST['comments'];
-  	$phonenumber = $_POST['phone'];
-
   	$db = mysqli_connect("studentdb-maria.gl.umbc.edu","ds14","ds14","ds14");
   	if(mysqli_connect_errno())
   		exit("Error - could not connect");
 
-  	$constructed_query = "INSERT INTO guestbook(username,comment_text,phone_number) VALUES ('$name','$comment','$phonenumber')";
+  	$constructed_query = "SELECT * FROM studyevent";
 
   	$result = mysqli_query($db,$constructed_query);
   	?>
