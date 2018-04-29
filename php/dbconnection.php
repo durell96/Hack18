d256de307ef3fbab9198d4886714929b30ab8013
durell96 <38799569+durell96@users.noreply.github.com>
2018-04-28 20:48:53 -0400

    Got rid of old php file new one works

---
+++ php/dbconnection.php
@@ -0,0 +1,33 @@
+<?php 
+  	$test = array("Psych", "EXAM2","2018-04-26", "10:00:00","ENG","GROUCH","STUDY PLEASE");
+
+	function addEvent($event) {
+	$db = mysqli_connect("studentdb-maria.gl.umbc.edu","ds14","ds14","ds14");
+  	if(mysqli_connect_errno()){
+	exit("Error - could not connect");
+	}
+    $constructed_query = "INSERT INTO studyevent(class,assignments,date,time,location,professor,description) VALUES ('$event[0]','$event[1]','$event[2]','$event[3]','$event[4]','$event[5]','$event[6]')";
+	$result = mysqli_query($db,$constructed_query);
+	mysqli_close($db);
+	} 
+	
+	
+	function selectEvent() {
+	$db = mysqli_connect("studentdb-maria.gl.umbc.edu","ds14","ds14","ds14");
+  	if(mysqli_connect_errno()){
+	exit("Error - could not connect");
+	}
+    $constructed_query = "SELECT * FROM studyevent";
+	$result = mysqli_query($db,$constructed_query);
+	$eventlist = array();
+	while($row = $result -> fetch_assoc())
+	{
+		$eventlist[] = $row;
+		
+	}
+	return $eventlist;
+	
+	mysqli_close($db);
+	} 
+	
+  	?>
